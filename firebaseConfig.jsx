15228bb8ea01f96f39269112fe706363c30a0fa2
Katie Sharples <KSharples431@gmail.com>
2024-06-26 12:24:24 -0400

    basic proj with vite, firebase, firebase hosting

---
+++ firebaseConfig.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+// Import other Firebase products you want to use, e.g., Auth
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Export the app or any services you need
+export default app;
