5101a523f37a7db1ef367a463f4b375002145f82
Katie Sharples <KSharples431@gmail.com>
2024-07-03 19:28:13 -0400

    minified images and added rollup plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), visualizer()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-redux': ['redux', 'react-redux'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 650, // Adjust chunk size limit for warnings
+  },
+});
