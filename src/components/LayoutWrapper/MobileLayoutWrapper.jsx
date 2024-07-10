b65154885a5a10f74c9fe984bfc3ef85255cc55d
Katie Sharples <KSharples431@gmail.com>
2024-07-10 15:40:07 -0400

    switched back to old mobilewrapper component

--- src/components/LayoutWrapper/MobileLayoutWrapper.jsx
+++ src/components/LayoutWrapper/MobileLayoutWrapper.jsx
@@ -6,43 +6,12 @@ import MobileHeader from '../Header/MobileHeader.jsx';
 const StyledMain = styled('main')(({ theme }) => ({
   flexGrow: 1,
   padding: theme.spacing(3),
-  paddingTop: theme.spacing(10), // Adjust this value based on your AppBar height
-  [theme.breakpoints.up('sm')]: {
-    paddingTop: theme.spacing(11),
-  },
-  display: 'flex',
-  flexDirection: 'column',
-  overflow: 'hidden', // Prevent content from overflowing
-}));
-
-const StyledContainer = styled(Container)(() => ({
-  flexGrow: 1,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  overflow: 'auto', // Allow scrolling if content is too large
-}));
-
-const ContentWrapper = styled('div')(() => ({
-  width: '100%',
-  maxHeight: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  transform: 'scale(1.05)', // Increase size by 5%
-  transformOrigin: 'center center',
-  '& > *': {
-    // Target direct children
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
+  paddingTop: theme.spacing(10),
+  [theme.breakpoints.up('sm')]: { paddingTop: theme.spacing(11) },
 }));
 
 const MobileLayoutWrapper = ({ children }) => {
   const theme = useTheme();
-
   return (
     <Box
       bgcolor={theme.palette.background.default}
@@ -53,11 +22,11 @@ const MobileLayoutWrapper = ({ children }) => {
       }}>
       <MobileHeader />
       <StyledMain>
-        <StyledContainer maxWidth="lg">
-          <ContentWrapper>{children}</ContentWrapper>
-        </StyledContainer>
+        <Container maxWidth="lg">{children}</Container>
       </StyledMain>
-      <Box component="footer" sx={{ py: 3, bgcolor: 'primary.main' }}>
+      <Box
+        component="footer"
+        sx={{ mt: 'auto', py: 3, bgcolor: 'primary.main' }}>
         <Container
           maxWidth="lg"
           bgcolor={theme.palette.background.default}>
@@ -72,6 +41,4 @@ const MobileLayoutWrapper = ({ children }) => {
 
 export default MobileLayoutWrapper;
 
-MobileLayoutWrapper.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+MobileLayoutWrapper.propTypes = { children: PropTypes.node.isRequired };
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import auth from '../../firebaseConfig.jsx'
 
-const API_URL = import.meta.env.VITE_API_URL
-// const API_URL = import.meta.env.VITE_API_URL_LOCAL
+// const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL_LOCAL
 
 const api = axios.create({
   baseURL: API_URL,
@@ -12,6 +12,7 @@ const api = axios.create({
 });
 
 const unprotectedRoutes = [
+  '/',
   '/books/',
   '/books/:bid',
   // '/users/signup',
